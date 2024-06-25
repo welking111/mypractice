2a871672cd1d508af687f3c19a88ce15f200003a
wangqing <wqing11@outlook.com>
2024-06-25 10:20:39 +0800

    wqing-Mac-2024.6.25

---
+++ helloword-app/src/com/wqing/test/test3.java
@@ -0,0 +1,12 @@
+package com.wqing.test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class test3 {
+    public static void main(String[] args) {
+        String str ="woshiyigehaochangde 数据";
+        System.out.println(str);
+
+    }
+}
